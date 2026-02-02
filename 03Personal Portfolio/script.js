d58ef25b3230679b192b7a4362cc698c5eb055fa
rxii <radityaarhb25@gmail.com>
2026-02-02 23:33:54 +0700

    commit

---
+++ 03Personal Portfolio/script.js
@@ -0,0 +1,23 @@
+const tgeme = localStorage.getItem("theme");
+if (theme) {
+     document.body.setAttribute("data-theme", theme);
+} else {
+     localStorage.setItem("theme", "dark");
+}
+
+function toggleTheme() {
+     const sunIcon = document.getElementById("sun-icon");
+     const moonIcon = document.getElementById("moon-icon");
+     const body = document.body;
+     const theme = body.getAttribute("data-theme");
+     body.setAttribute("data-theme", theme === "dark" ? "light" : "dark");
+          if (theme === "dark") {
+               sunIcon.style.display = "block";
+               moonIcon.style.display = "none";
+          }
+          else {
+               sunIcon.style.display = "none";
+               moonIcon.style.display = "block";
+          }
+     }
+
